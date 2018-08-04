9d758920bf8e74326971f1d810b49525343e7555
David Frederick <88cassette@gmail.com>
2018-08-03 20:03:19 -0400

    Added Heroku ssl redirect

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ var express         = require("express"),
     bodyParser      = require("body-parser");
 
 
+const sslRedirect = require('heroku-ssl-redirect');
+
 const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(sslRedirect());
+
+
 
 
 app.get('/', function(req, res) {
