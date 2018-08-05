c1a467e43e63ba136aa01d6c04272e227df8969b
David Frederick <88cassette@gmail.com>
2018-08-04 20:21:29 -0400

    grid work plus storefront

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var express         = require("express"),
     app             = express(),
-    bodyParser      = require("body-parser");
-
+    bodyParser      = require("body-parser"),
+    favicon         = require('serve-favicon'),
+    path            = require('path');
 
 const sslRedirect = require('heroku-ssl-redirect');
-
 const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use('/favicon.ico', express.static('images/favicon.png'));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
 app.use(sslRedirect());
 
 
@@ -16,6 +18,10 @@ app.use(sslRedirect());
 
 app.get('/', function(req, res) {
     res.render("landing");
+})
+
+app.get('/test', function(req, res) {
+    res.render("splash");
 })
 
 
